4a1df2fc93a272c1d64b65f67fbf2cd270f5e756
David Arvidsson <david.arvidsson@hotmail.com>
2024-08-16 20:29:08 +0000

    Added gunicorn to requirements.txt

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-STATUS = (
-    (0, 'Draft'),
-    (1, 'Published'),
-)
-
 # Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -16,4 +14,4 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    STATUS = ((0, "Draft"), (1, "Published"))
+    excerpt = models.TextField(blank=True)
